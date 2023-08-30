f399886e1aba332cfb034538ec522b2741bfb5d5
jhnnsrs <jhnnsrs@gmail.com>
2023-08-30 15:24:48 +0200

    uff

--- src/herre/index.tsx
+++ src/herre/index.tsx
@@ -1,7 +1,7 @@
 import { HerreContextType, useHerre } from "./HerreContext";
-import { HerreProvider, HerreProps } from "./HerreProvider";
+import { HerreProvider, HerreProps, windowRedirect } from "./HerreProvider";
 import { HerreGuard, herreGuarded } from "./HerreGuard";
 import { Token, Scopes } from "./types";
 
-export { HerreGuard, HerreProvider, useHerre, herreGuarded };
+export { HerreGuard, HerreProvider, useHerre, herreGuarded, windowRedirect };
 export type { HerreContextType, HerreProps, Token, Scopes };
