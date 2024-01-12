37aaaa0db2c6515d4e0ab27a707f7af1bdf8237e
jhnnsrs <jhnnsrs@gmail.com>
2024-01-12 15:04:44 +0100

    fixed use login

--- src/herre/hooks/useLogin.tsx
+++ src/herre/hooks/useLogin.tsx
@@ -2,73 +2,61 @@ import { useCallback, useState } from "react";
 import { CancelableRequest, LoginRequest, useHerre } from "../HerreContext";
 import { Token } from "../types";
 
+export const useLogin = () => {
+  const { token, login: herreLogin, logout: herreLogout } = useHerre();
 
-export const useLogin = (request: LoginRequest) => {
+  const [progress, setProgress] = useState<string | undefined>(undefined);
+  const [error, setError] = useState<string | undefined>(undefined);
+  const [promise, setPromise] = useState<CancelableRequest<Token> | null>(null);
 
-    const { token, login: herreLogin, logout: herreLogout } = useHerre();
+  const login = useCallback(
+    (request: LoginRequest) => {
+      if (promise) {
+        promise.cancel();
+      } else {
+        let causedRequest = { ...request };
 
-    const [progress, setProgress] = useState<string | undefined>(undefined);
-    const [error, setError] = useState<string | undefined>(undefined);
-    const [promise, setPromise] = useState<CancelableRequest<Token> | null>(null);
-   
+        setError(undefined);
+        const newPromise = herreLogin({
+          ...causedRequest,
+          onProgress: (progress) => {
+            causedRequest.onProgress && causedRequest.onProgress(progress);
+            setProgress(progress);
+          },
+        });
 
-    const login = useCallback((other: Partial<LoginRequest> = {}) => {
-        if (promise) {
-            promise.cancel();
-        }
-        else {
+        newPromise.promise
+          .then((x) => {
+            setPromise(null);
+            setProgress(undefined);
+          })
+          .catch((e) => {
+            console.log("Error", e);
 
-            let causedRequest = {...request, ...other}
+            setPromise(null);
+            setError(e.message);
+          });
 
-            setError(undefined)
-            const newPromise = herreLogin({
-                ...causedRequest, 
-                onProgress: (progress) => {
-                    causedRequest.onProgress && causedRequest.onProgress(progress)
-                    setProgress(progress);
-                }
-            });
-            
-            newPromise.promise.then(x => {
-                setPromise(null)
-                setProgress(undefined)
-            
-            
-            }).catch((e) => {
-                console.log("Error", e);
-                
-                setPromise(null)
-                setError(e.message)
-            })
+        setPromise(newPromise);
+      }
+    },
+    [promise, herreLogin]
+  );
 
-
-
-            setPromise(newPromise)
-        }
+  const logout = useCallback(() => {
+    if (promise) {
+      promise.cancel();
+    } else {
+      herreLogout();
     }
-    , [promise, herreLogin, request]);
+  }, [promise, herreLogout]);
 
-
-    const logout = useCallback(() => {
-        if (promise) {
-            promise.cancel();
-        }
-        else {
-            herreLogout()
-        }
-    }, [promise, herreLogout])
-
-    return {
-        progress,
-        loading : promise != null,
-        login,
-        logout,
-        error,
-        token
-    }
-    
-
-
-
-
-}
+  return {
+    progress,
+    loading: promise != null,
+    login,
+    logout,
+    error,
+    token,
+  };
+};
